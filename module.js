da0364e3185dace3dcf98397e05372a6ae7f879d
kevinwang349 <53480441+kevinwang349@users.noreply.github.com>
2021-08-08 18:56:08 -0400

    Delete module.js

--- module.js
+++
@@ -1,9 +0,0 @@
-function exe() {
-    import * as https from 'https';
-    const url = 'https://random-d.uk/';
-    https.get(url, response => {
-        response.on('data', (data) => {
-            console.log(data.toString());
-        });
-    });
-}
