8d7331d3a1485fca9092f76ca57f7ff405461589
kevinwang349 <53480441+kevinwang349@users.noreply.github.com>
2021-08-08 18:55:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', init);
-//import * as fs from 'fs';
 function init() {
     exe();
+}
+function exe() {
+    import * as https from https;
+    const url = 'https://random-d.uk/';
+    https.get(url, response => {
+        response.on('data', (data) => {
+            console.log(data.toString());
+        });
+    });
 }
 /*const fs = require('fs');
 const http = require('http');
